4ba2571a14425345ca320118eef5d4cb7e96f1be
JhonGalante <jhonata.galante@gmail.com>
2019-10-20 10:55:42 -0300

    Criado service de usuario

--- src/main/java/webservice/UsuarioRest.java
+++ src/main/java/webservice/UsuarioRest.java
@@ -5,11 +5,17 @@
  */
 package webservice;
 
+import com.google.gson.Gson;
 import dao.UsuarioDAO;
+import java.util.List;
 import javax.inject.Named;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import model.Usuario;
@@ -23,9 +29,10 @@ import model.Usuario;
 @Path("usuario")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class UsuarioRest {
+public class UsuarioRest{
     
-    private UsuarioDAO dao = UsuarioDAO.getInstance();
+    private final UsuarioDAO dao = UsuarioDAO.getInstance();
+    private final Gson gson = new Gson();
     
     @GET
     @Path("helloworld")
@@ -33,4 +40,16 @@ public class UsuarioRest {
         Usuario usuario = dao.buscarMatricula("333");
         return usuario.getNome();
     }
+    
+    @GET
+    @Path("buscar-por-id/{matricula}")
+    public String buscarPorId(@PathParam("matricula") String matricula){
+        Usuario usuario = dao.buscarMatricula(matricula);
+        if(usuario != null){
+            String json = gson.toJson(usuario);
+            return json;
+        }
+        return null;
+    }
+    
 }
