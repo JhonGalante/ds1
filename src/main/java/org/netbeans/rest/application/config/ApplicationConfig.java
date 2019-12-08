a79a79432ecd95ed55a388dc6628c175e90b48c3
Jhonata Galante <jhonata.galante@gmail.com>
2019-12-08 21:16:29 -0200

    CrossOrigin

--- src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -30,6 +30,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(webservice.MovimentacaoRest.class);
+        resources.add(webservice.NewCrossOriginResourceSharingFilter.class);
         resources.add(webservice.ProfessorRest.class);
         resources.add(webservice.TccRest.class);
         resources.add(webservice.TermoRest.class);
