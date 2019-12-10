60066d9dc430488d3a0bcc74354838b38395176b
Jhonata <jhonata.galante@gmail.com>
2019-12-10 10:52:30 -0300

    Fix CORS webservice

--- src/main/java/webservice/NewCrossOriginResourceSharingFilter.java
+++ src/main/java/webservice/NewCrossOriginResourceSharingFilter.java
@@ -5,6 +5,7 @@
  */
 package webservice;
 
+import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -18,10 +19,13 @@ import javax.ws.rs.ext.Provider;
 public class NewCrossOriginResourceSharingFilter implements ContainerResponseFilter {
 
     @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext response) {
-        response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
-        response.getHeaders().putSingle("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
-        response.getHeaders().putSingle("Access-Control-Allow-Headers", "Content-Type");
-    }
+   public void filter(final ContainerRequestContext requestContext,
+                      final ContainerResponseContext cres) throws IOException {
+      cres.getHeaders().add("Access-Control-Allow-Origin", "*");
+      cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+      cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
+      cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+      cres.getHeaders().add("Access-Control-Max-Age", "1209600");
+   }
     
 }
